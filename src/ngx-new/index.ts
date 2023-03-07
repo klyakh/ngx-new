5c9f88a2fc099ef3dbe5291e63532eb563be5ced
Konstantin Lyakh <kostya.lyakh@gmail.com>
2023-03-08 02:19:35 +0300

    Progress.

--- src/ngx-new/index.ts
+++ src/ngx-new/index.ts
@@ -4,6 +4,9 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function ngxNew(_options: any): Rule {
+  
+  console.log('Hello from your new schematic!');
+
   return (tree: Tree, _context: SchematicContext) => {
     return tree;
   };
